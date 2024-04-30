f63feeadac3926f6a5023d9edfd4e69842a5bb72
rohitw20 <rohit.w@aikyamedge.com>
2024-04-30 18:04:09 +0530

    gsap animation added to navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,15 +1,58 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./Navbar.css";
 import { SiWebmoney } from "react-icons/si";
 import { menu } from "../../data";
-import { Link } from "react-scroll";
+import { Link, animateScroll as scroll } from "react-scroll";
 import { FaArrowUpRightFromSquare, FaBarsStaggered } from "react-icons/fa6";
 import { FaTimes } from "react-icons/fa";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 
 const Navbar = () => {
   const [showSidebar, setShowSidebar] = useState(false);
+  const [visible, setVisible] = useState(false);
+  const handleScroll = () => {
+    const currentScrollPos = window.scrollY;
+    return setVisible(currentScrollPos > 145);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  const container = useRef(null);
+  gsap.registerPlugin(useGSAP);
+  useEffect(() => {
+    if (visible) {
+      gsap.fromTo(
+        ".navbar__container",
+        {
+          y: -250,
+          width: "100%",
+        },
+        {
+          y: 0,
+          top: 0,
+          zIndex: 100,
+        }
+      );
+    }
+  }, [visible]);
+
+  useGSAP(
+    () => {
+      const timeline = gsap.timeline();
+      timeline.from(".tab__item", { opacity: 0, stagger: 0.5 });
+    },
+    { scope: container }
+  );
+
   return (
-    <nav className="navbar__container">
+    <nav
+      className={`navbar__container ${visible ? "visible" : ""}`}
+      ref={container}
+    >
       {showSidebar ? (
         <div
           className="overlay"
@@ -18,7 +61,10 @@ const Navbar = () => {
       ) : (
         ""
       )}
-      <div className="logo__container">
+      <div
+        className="logo__container"
+        onClick={() => scroll.scrollToTop({ duration: 500 })}
+      >
         <SiWebmoney />
       </div>
       <div className={`tab__group ${showSidebar ? "show" : ""}`}>
