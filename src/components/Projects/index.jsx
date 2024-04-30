1ba19abb08f0817c8f3f0a5b75061c976fba3598
rohitw20 <rohit.w@aikyamedge.com>
2024-04-30 12:41:29 +0530

    projects section added

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -1,8 +1,83 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./Projects.css";
+import { sumArray } from "../../helper";
+import { projects } from "../../data";
+import Card from "./Card";
+
+const tabs = [
+  { name: "All" },
+  { name: "Web" },
+  { name: "UI/UX" },
+  { name: "Apps" },
+];
 
 const Projects = () => {
-  return <div>Projects</div>;
+  const [displayableProjects, setDisplayableProjects] = useState(projects);
+
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [offset, setOffset] = useState(0);
+  const [indicatorWidth, setIndicatorWidth] = useState(0);
+  const itemsEls = useRef(new Array());
+
+  useEffect(() => {
+    const prevEl = itemsEls.current.filter((_, index) => index < activeIndex);
+    setOffset(sumArray(prevEl.map((item) => item.offsetWidth)));
+    setIndicatorWidth(itemsEls.current[activeIndex].offsetWidth);
+  }, [activeIndex]);
+
+  const setProjects = (category) => {
+    if (category === "All") return setDisplayableProjects(projects);
+
+    const pro = projects.filter(
+      (item) =>
+        item.category.toLocaleLowerCase() === category.toLocaleLowerCase()
+    );
+    setDisplayableProjects(pro);
+  };
+
+  return (
+    <section id="projects">
+      <div className="section__wrapper projects__container">
+        <div className="section__header center">
+          <h2 className="primary__title">Projects</h2>
+        </div>
+        <nav>
+          {tabs.map((tab, index) => (
+            <button
+              className=""
+              onClick={() => {
+                setActiveIndex(index);
+                setProjects(tab.name);
+              }}
+              ref={(el) => (itemsEls.current[index] = el)}
+              key={index}
+            >
+              {tab.name}
+            </button>
+          ))}
+          <span
+            className="active__indicator"
+            style={{
+              left: `${offset}px`,
+              width: `${indicatorWidth}px`,
+            }}
+          ></span>
+        </nav>
+
+        <div className="card__container">
+          {displayableProjects.map((project, index) => (
+            <Card
+              title={project.title}
+              image={project.image}
+              data={project.data}
+              stack={project.stack}
+              key={index}
+            />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Projects;
